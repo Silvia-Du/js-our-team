ac7f5b4914bf3dbebe1d58a168af81a4b297da54
Silvia-Du <silviadu@hotmail.it>
2022-04-27 15:37:47 +0200

    Fine test raccolta dati dall'utente con Input

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const teamContainer = document.querySelector('.team-container');
-
-
+const buttonAddMember = document.getElementById('addMemberButton');
+//array team
 const team = [
   {
     imageUrl: 'img/angela-caroll-chief-editor.jpg',
@@ -21,14 +21,41 @@ const team = [
   {
     imageUrl: 'img/wayne-barnett-founder-ceo.jpg',
     name: 'Wayne Barnett',
-    ruolo: 'Chief'
+    ruolo: 'Founder Ceo'
   },
+  {
+    imageUrl: 'img/walter-gordon-office-manager.jpg',
+    name: 'Walter Gordon',
+    ruolo: 'Office Manager'
+  },
+  {
+    imageUrl: 'img/scott-estrada-developer.jpg',
+    name: 'Scott Estrada',
+    ruolo: 'Uman Resource Director'
+  },
+  {
+    imageUrl: 'img/new-team-member-04.jpg',
+    name: 'Marck Jacobs',
+    ruolo: 'Developer Project Manager'
+  },
+  {
+    imageUrl: 'img/new-team-member-03.jpg',
+    name: 'Suzanee Waite',
+    ruolo: 'Direttrice Operativa'
+  },
+  {
+    imageUrl: 'img/new-team-member-02.jpg',
+    name: 'Wayne Barnett',
+    ruolo: 'Chiaramente Aviatore aziendale'
+  },
+  //non metto l'ultimo per usare l'img come prova utente
 ];
 
 console.log(team);
 
+
+//stampo in pagina le card
 for(let i in team){
-  console.log('simgolo membro del team----',team[i]);
 
   const outputCard = `
     <div class="team-card">
@@ -45,5 +72,20 @@ for(let i in team){
     </div>
   `;
   teamContainer.innerHTML += outputCard;
+}
+
+buttonAddMember.addEventListener('click', takeNewMember);
+
+function takeNewMember(){
+
+  const name = document.getElementById('name').value.trim();
+  console.log(name,'nome user');
+  const role = document.getElementById('role').value.trim();
+  console.log(role, 'ruolo utente');
+  const imageUser = document.getElementById('image').value.trim();
+  console.log(imageUser);
+
+  
+
 }
 
